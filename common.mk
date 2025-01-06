ecb304061ca902e7215666642fa278b670e476b9
LeonLeonPotato <leonzhu2007@gmail.com>
2025-01-06 16:27:48 +0800

    faster math

--- common.mk
+++ common.mk
@@ -1,7 +1,8 @@
 ARCHTUPLE=arm-none-eabi-
 DEVICE=VEX EDR V5
 
-MFLAGS=-mcpu=cortex-a9 -mfpu=neon-fp16 -mfloat-abi=hard -O3 -g -ffast-math -ftree-vectorize
+# add -funsafe-math-optimizations maybe
+MFLAGS=-mcpu=cortex-a9 -mfpu=neon-fp16 -mfloat-abi=hard -O3 -g -ffast-math -ftree-vectorize -fno-math-errno
 CPPFLAGS=-D_POSIX_THREADS -D_UNIX98_THREAD_MUTEX_ATTRIBUTES -D_POSIX_TIMERS -D_POSIX_MONOTONIC_CLOCK -DEIGEN_USE_NEON -DEIGEN_VECTORIZE_BFLOAT16
 GCCFLAGS=-ffunction-sections -fdata-sections -fdiagnostics-color -funwind-tables
 
--- include/gui/fonts/roboto_regular_16.c
+++ include/gui/fonts/roboto_regular_16.c
@@ -829,11 +829,7 @@ extern const lv_font_t lv_font_montserrat_20;
  *----------------*/
 
 /*Initialize a public general font descriptor*/
-#if LVGL_VERSION_MAJOR >= 8
 const lv_font_t roboto_regular_16 = {
-#else
-lv_font_t roboto_regular_16 = {
-#endif
     .get_glyph_dsc = lv_font_get_glyph_dsc_fmt_txt,    /*Function pointer to get glyph's data*/
     .get_glyph_bitmap = lv_font_get_bitmap_fmt_txt,    /*Function pointer to get glyph's bitmap*/
     .line_height = 19,          /*The maximum line height required by the font*/
