5a656f2fa9812187d62f9280c4b2c814c96ad489
dyisb <130207284+dyisb@users.noreply.github.com>
2025-09-25 17:27:03 +0800

    Update strategies.cpp

--- src/autonomous/strategies.cpp
+++ src/autonomous/strategies.cpp
@@ -4,27 +4,28 @@
 #include <iostream>
 
 using namespace strategies;
+using namespace std;
 
 static void none(void) {
-    std::cout << PREFIX << "No strategy chosen" << std::endl;
+    cout << PREFIX << "No strategy chosen" << std::endl;
     pros::delay(500);
 }
 
-const std::map<Strategy, std::string> strategies::names = {
+const map<Strategy, std::string> strategies::names = {
     {Strategy::Test, "Test"},
     {Strategy::MPCStrat, "MPC Strat"},
     {Strategy::Skills, "Skills"},
     {Strategy::None, "None"}
 };
 
-const std::map<std::string, Strategy> strategies::values = {
+const map<std::string, Strategy> strategies::values = {
     {"Test", Strategy::Test},
     {"MPC Strat", Strategy::MPCStrat},
     {"Skills", Strategy::Skills},
     {"None", Strategy::None}
 };
 
-const std::map<Strategy, void (*)()> strategies::functions = {
+const map<Strategy, void (*)()> strategies::functions = {
     {Strategy::Test, test_strategy::run},
     {Strategy::MPCStrat, mpcstrat::run},
     {Strategy::Skills, skills::run},
